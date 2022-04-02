ad04bc564e54da674d2aaf90bce15dd4ab96986d
xan <47989552+style-x@users.noreply.github.com>
2022-04-02 17:02:01 +0200

    Players Turn
    Xs werden gesetzt, jedoch funktioniert der automatische Spielerwechsel noch nicht..

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 
-const cell = document.getElementsByClassName('cell');
+const cell = document.querySelectorAll('.cell');
+
+let pt = "x";
+
+cell.forEach(el => {
+  el.addEventListener('click', player);
+});
+
+function player(e) {
+  console.log(e);
+  if ( this.classList.contains('x') || this.classList.contains('o') ) {
+    return;
+  } else {
+    this.classList.add(pt);
+    if (pt == "x") { pt = "o" };
+    if (pt == "o") { pt = "x" };
+    console.log(pt);
+  }
+};
 
 function gameover() {
   console.log("Game Over");
